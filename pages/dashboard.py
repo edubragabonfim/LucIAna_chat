89e74b90fca0da876e95233782cbe52f3dc2d759
edubragabonfim <eduardo.bonfim.dub@gmail.com>
2024-09-04 18:23:06 -0300

    Test 12

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -13,6 +13,12 @@ st.sidebar.write('This is a experiment')
 st.header('Dashboard', divider='blue')
 
 div = f"""
-<iframe title="Dashbirds - LucIAna" width="2000" height="1100" src="https://app.powerbi.com/view?r=eyJrIjoiMjQ0N2IwZGUtNTdhZC00MTg2LWI4NDItYmM0MDdmZGUzY2IzIiwidCI6ImUxNjhhZTdjLWY5ZmMtNDIwZi04ODM4LTUwNmQxY2NlNWMwMiJ9" frameborder="0" allowFullScreen="true"></iframe>
+<iframe title="Dashbirds - LucIAna" 
+        width="100%" 
+        height="100%" 
+        style="min-width: 300px; min-height: 1000px; border: none;" 
+        src="https://app.powerbi.com/view?r=eyJrIjoiMjQ0N2IwZGUtNTdhZC00MTg2LWI4NDItYmM0MDdmZGUzY2IzIiwidCI6ImUxNjhhZTdjLWY5ZmMtNDIwZi04ODM4LTUwNmQxY2NlNWMwMiJ9" 
+        allowFullScreen="true">
+</iframe>
 """
 st.markdown(div, unsafe_allow_html=True)
