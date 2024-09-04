a63e67348ef4d0ddf658e769f6d39ca1af78de3e
edubragabonfim <eduardo.bonfim.dub@gmail.com>
2024-09-04 14:35:13 -0300

    Nada de muito importante com o código. Testando para ver se as mudanças refletem no railway automaticamente

--- main.py
+++ main.py
@@ -33,31 +33,56 @@ df_users_query = '''
     '''
 df_users = sqlio.read_sql_query(df_users_query, conn)
 
+st.set_page_config(
+    'Admin LucIAna',
+    '🦅',
+    layout='centered',
+    initial_sidebar_state='collapsed'
+)
 
 # Here is the start of the site
+# First, we create the sidebar
+st.sidebar.write('Sup')
+
 st.header('LucIAna Admin 🧠⚙', divider='blue')
 
+# Indicadores
 col1, col2 = st.columns(2)
 col1.metric("Users", df_users.shape[0])
 col2.metric("Last User Created", df_users['name'].head(1).unique()[0])
+st.dataframe(df_users.head(3), hide_index=True)  # Exibe os últimos 3 usuários que entraram
 
 st.header('Users', divider='blue')
 if st.button('Render Users', key='render_users'):
-    st.dataframe(df_users, hide_index=True)
+    st.dataframe(df_users.sort_values('_id_user', ascending=True), hide_index=True)
 
 st.divider()
 
+
+# Set configs container
 st.header('Set Configs', divider='blue')
+dropdown_userselected = st.selectbox('User Selected', df_users['_id_user'].unique(), index=0)
+st.session_state.userselected = dropdown_userselected
+    #Exibe  as configurações do usuário selecionado
+st.write('User Selected:', st.session_state.userselected)
+# st.write('Model:', st.session_state.userselected)
 
-dropdown_userselected = st.selectbox('User Selected', df_users['_id_user'].unique())
-dropdown_typeuser = st.selectbox('Type User', ['admin', 'user'])
-dropdown_featuresavailable = st.selectbox('Features Available', ['all', 'few'])
 
+# Divide em colunas para poder dizer qual opção quer mudar ou não
+col1, col2 = st.columns(2)
+toggle_typeuser_option = col1.toggle("Type User")
+toggle_featuresavailable_option = col1.toggle("Features Available")
+
+# DropDowns
+if toggle_typeuser_option:
+    dropdown_typeuser = col2.selectbox('Type User', ['admin', 'user'])
+    st.session_state.typeuser = dropdown_typeuser
+if toggle_featuresavailable_option:
+    dropdown_featuresavailable = col2.selectbox('Features Available', ['all', 'few'])
+    st.session_state.featuresavailable = dropdown_featuresavailable
+
+# Se o botão for pressionado, faça...
 if st.button('Submit', key='submit_changes'):
-    st.write(f'dropdown_userselected: {dropdown_userselected}')
-    st.write(f'dropdown_typeuser: {dropdown_typeuser}')
-    st.write(f'dropdown_featuresavailable: {dropdown_featuresavailable}')
-
     # Update Features
     sql_update_featuresavailable_query = """UPDATE public.gpt_users
                         SET features_available = %s
@@ -75,9 +100,9 @@ if st.button('Submit', key='submit_changes'):
     # Commit the changes to the database
     conn.commit()
 
+    # Exibe a linha do usuário alterado, no banco
     df_users = sqlio.read_sql_query(df_users_query, conn)
-
     st.dataframe(df_users.query(f'_id_user == {dropdown_userselected}'), hide_index=True)
 
-st.info(st.session_state)
 
+st.sidebar.json(st.session_state, expanded=True)
