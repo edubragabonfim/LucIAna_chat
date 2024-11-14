71e788db2266875dfcc31570afc4a2a4864ea134
edubragabonfim <eduardo.bonfim.dub@gmail.com>
2024-11-14 07:54:37 -0300

    Att

---
+++ views/main.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+import streamlit as st
+import streamlit_authenticator as stauth
+
+def page_main():
+    link = 'http://wa.me/5531973513292'
+
+    st.sidebar.write(f"Chat with LucIAna in [WhatsApp]({link})")
+    st.sidebar.json(st.session_state, expanded=False)
+    st.header('LucIAna Portal 🧠', divider='blue')
+    # st.image('https://drive.google.com/file/d/1cXhnzARrCVFpsDjSp7-qWQ4odTPNalln/view')
+    st.image('static/LucIAna.jpg')
+
---
+++ views/messages.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import psycopg2
+import pandas as pd
+import pandas.io.sql as sqlio
+import os
+from dotenv import load_dotenv
+
+def page_messages():
+    load_dotenv()
+
+    conn = psycopg2.connect(
+        user=os.getenv('PG_USER'),
+        password=os.getenv('PG_PWD'),
+        host=os.getenv('PG_HOST'),
+        port=os.getenv('PG_PORT'),
+    )
+
+    cur = conn.cursor()
+
+
+    # @st.cache_data
+    def load_data():
+        query = '''
+            select
+                *
+                , length(prompt) as prompt_length
+                , length(response) as response_length
+            from public.gpt_messages gm 
+            order by gm."_id_message" desc
+            '''
+        df = sqlio.read_sql_query(query, conn)
+        return df
+
+
+    if st.button('Query Messages'):
+        df_messages = load_data()
+        col1, col2 = st.columns(2)
+        col1.metric("Messages", df_messages.shape[0])
+        st.dataframe(df_messages, hide_index=True)
